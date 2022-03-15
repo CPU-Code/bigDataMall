f57f55e1d6ed5f78cbcdb67ce10ded22a9968d29
cpu_code <54974321+CPU-Code@users.noreply.github.com>
2022-03-15 22:36:55 +0800

    日志表

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # bigDataMall
-bigDataMall
+
+
+电商数仓
+
+技术栈 : 
+
+* Flume : 日志收集
+* Kafka : 实时缓存
+* DataX : 全量同步
+* Maxwell : 增量同步
+* Hive : 查询
+* Spark : 计算引擎
+* HDFS : 存储
+* YARN : 资源调度
+* Zookeeper : HA选举
+* JDK8 : Java环境
+* Python : 脚本
+* Shell : 脚本
+
+
+分层 : 
+
+* ods : 原始数据层
+* dim 公共维度层
+* dwd 明细数据层
+* dws 汇总数据层
+* aps 数据应用层
+
+
+
+----------------
+
+# [1 原始数据层](ods)
+
+
+## [1.1 日志表](ods/log)
+
+- [x] [日志表](ods/log/ods_log_inc.sql)
+
+
+## [1.2 全量表](ods/full)
+
+- [x] [活动信息表](ods/full/ods_activity_info_full.sql)
+
+
+
+
+## [1.3 增量表](ods/inc)
+
+- [ ] [](ods/full/)
+
+
+
+----------------
+
+# [公共维度层](dim)
+
+
+
+
+
+----------------
+
+# [明细数据层](dwd)
+
+
+
+
+
+
+
+----------------
+
+# [汇总数据层](dws)
+
+
+
+
+
+
+----------------
+
+# [数据应用层](aps)
+
+
+
+
---
+++ ods/log/ods_log_inc.sql
@@ -0,0 +1,48 @@
+
+
+-- 删除表
+
+DROP TABLE IF EXISTS ods_log_inc;
+
+
+-- 创建外部表
+
+CREATE EXTERNAL TABLE ods_log_inc
+(
+    `common`   STRUCT<ar :STRING, ba :STRING, ch :STRING, is_new :STRING, md :STRING, mid :STRING, os :STRING, uid
+                      :STRING, vc :STRING> COMMENT '公共信息',
+    `page`     STRUCT<during_time :BIGINT, item :STRING, item_type :STRING, last_page_id :STRING, page_id :STRING,
+                      source_type :STRING> COMMENT '页面信息',
+    `actions`  ARRAY<STRUCT<action_id:STRING, item:STRING, item_type:STRING, ts:BIGINT>> COMMENT '动作信息',
+    `displays` ARRAY<STRUCT<display_type :STRING, item :STRING, item_type :STRING, `order` :STRING, pos_id
+                            :STRING>> COMMENT '曝光信息',
+    `start`    STRUCT<entry :STRING, loading_time :BIGINT, open_ad_id :BIGINT, open_ad_ms :BIGINT, open_ad_skip_ms
+                      :BIGINT> COMMENT '启动信息',
+    `err`      STRUCT<error_code:BIGINT, msg:STRING> COMMENT '错误信息',
+    `ts`       BIGINT COMMENT '时间戳'
+) COMMENT '活动信息表'
+    PARTITIONED BY (`dt` STRING)
+    ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.JsonSerDe'
+    LOCATION '/warehouse/gmall/ods/ods_log_inc/';
+
+
+--- 转载数据
+
+load data inpath '/origin_data/gmall/log/topic_log/2020-06-14' into table ods_log_inc partition (dt = '2020-06-14');
+
+
+--- 查询数据
+
+
+select common,
+       page,
+       actions,
+       displays,
+       `start`,
+       err,
+       ts,
+       dt
+from ods_log_inc
+where dt = '2020-06-14'
+
+

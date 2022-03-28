3fe195598c332970a7857fecce5c4d0180645f95
cpuCode <923992029@qq.com>
2022-03-28 22:37:41 +0800

    流量域访客页面粒度页面浏览最近1日汇总表

--- README.md
+++ README.md
@@ -277,6 +277,7 @@ CSDN : [https://blog.csdn.net/qq_44226094/category_11631933.html?spm=1001.2014.3
 - [ ] [交易域优惠券粒度订单最近n日汇总表](dws/nd/trade/dws_trade_coupon_order_nd.sql)
 - [ ] [交易域活动粒度订单最近n日汇总表](dws/nd/trade/dws_trade_activity_order_nd.sql)
   
+
 - [ ] [流量域访客页面粒度页面浏览最近n日汇总表](dws/nd/traffic/dws_traffic_page_visitor_page_view_nd.sql)
 
 
@@ -284,6 +285,7 @@ CSDN : [https://blog.csdn.net/qq_44226094/category_11631933.html?spm=1001.2014.3
 
 - [ ] [交易域用户粒度订单历史至今汇总表](dws/td/trade/dws_trade_user_order_td.sql)
 - [ ] [交易域用户粒度支付历史至今汇总表](dws/td/trade/dws_trade_user_payment_td.sql)
+
 
 - [ ] [用户域用户粒度登录历史至今汇总表](dws/td/user/dws_user_user_login_td.sql)
 
--- dws/1d/traffic/dws_traffic_page_visitor_page_view_1d.sql
+++ dws/1d/traffic/dws_traffic_page_visitor_page_view_1d.sql
@@ -1,15 +1,57 @@
-
-
-
 -- 创建 流量域访客页面粒度页面浏览最近1日汇总表
+
+drop table if exists dws_traffic_page_visitor_page_view_1d;
+
+create external table dws_traffic_page_visitor_page_view_1d
+(
+    `mid_id`         STRING COMMENT '访客id',
+    `brand`          string comment '手机品牌',
+    `model`          string comment '手机型号',
+    `operate_system` string comment '操作系统',
+    `page_id`        STRING COMMENT '页面id',
+    `during_time_1d` BIGINT COMMENT '最近1日浏览时长',
+    `view_count_1d`  BIGINT COMMENT '最近1日访问次数'
+) comment '流量域访客页面粒度页面浏览最近1日汇总事实表'
+    partitioned by (`dt` string)
+    stored as orc
+    location '/warehouse/gmall/dws/dws_traffic_page_visitor_page_view_1d'
+    tblproperties ('orc.compress' = 'snappy');
 
 
 -- 每日装载 流量域访客页面粒度页面浏览最近1日汇总表
 
+insert overwrite table dws_traffic_page_visitor_page_view_1d
+    partition (dt = '2020-06-14')
+select mid_id,
+       brand,
+       model,
+       operate_system,
+       page_id,
+       sum(during_time),
+       count(*)
+from dwd_traffic_page_view_inc
+where dt = '2020-06-14'
+group by mid_id, brand, model, operate_system, page_id;
 
 
 -- 查看分区
 
+show partitions dwd_traffic_page_view_inc;
 
 
 -- 查看 流量域访客页面粒度页面浏览最近1日汇总表
+
+select mid_id,
+       brand,
+       model,
+       operate_system,
+       page_id,
+       during_time_1d,
+       view_count_1d,
+       dt
+from dws_traffic_page_visitor_page_view_1d
+where dt = '2020-06-14'
+group by mid_id, brand, model, operate_system, page_id;
+
+
+

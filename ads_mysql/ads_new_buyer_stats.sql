d2cc2f973d4e0c61f29c5684d123210077df03b7
cpuCode <923992029@qq.com>
2022-04-04 13:29:59 +0800

    新增交易用户统计

--- ads_mysql/ads_new_buyer_stats.sql
+++ ads_mysql/ads_new_buyer_stats.sql
@@ -11,6 +11,5 @@ create table `ads_new_buyer_stats`
     PRIMARY KEY (`dt`, `recent_days`) USING BTREE
 ) engine = InnoDB
   character set = utf8
-  collate = utf8_general_ci
-    comment = '新增交易用户统计'
+  collate = utf8_general_ci comment = '新增交易用户统计'
   row_format = Dynamic;
